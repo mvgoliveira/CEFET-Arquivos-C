7ec9a658dcf2122eb44d91c62fe66400ec06771f
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-08-03 02:23:05 -0300

    v2

--- ExternalSort.c
+++ ExternalSort.c
@@ -5,55 +5,94 @@
 typedef struct _Endereco Endereco;
 
 struct _Endereco {
-	char logradouro[72];
-	char bairro[72];
-	char cidade[72];
-	char uf[72];
-	char sigla[2];
-	char cep[8];
-	char lixo[2];
+  char logradouro[72];
+  char bairro[72];
+  char cidade[72];
+  char uf[72];
+  char sigla[2];
+  char cep[8];
+  char lixo[2];
 };
 
-int compara(const void *e1, const void *e2) {
-	return strncmp(((Endereco*)e1)->cep,((Endereco*)e2)->cep,8);
+void ordenacao(long, int, FILE *);
+void intercala(char *, char *, char *);
+void teste(char *);
+char *criaNomeArquivosIntercala(int);
+
+int main(void) {
+  FILE *entrada;
+  long tamanho, qtdRegistros;
+  int qtdArq;
+  char nomeArqResultante[128];
+
+  printf("Digite a quantidade de divisões no arquivo: ");
+  scanf("%d", &qtdArq);
+
+  if (qtdArq % 2 != 0) {
+    printf("Qtd de arquivos precisa ser um número par");
+    return 0;
+  }
+
+  entrada = fopen("cep.dat", "rb");
+
+  fseek(entrada, 0, SEEK_END);
+
+  tamanho = ftell(entrada);
+  qtdRegistros = tamanho / sizeof(Endereco);
+
+  rewind(entrada);
+
+  ordenacao(qtdRegistros, qtdArq, entrada);
+
+  fclose(entrada);
+
+  sprintf(nomeArqResultante, "%s", criaNomeArquivosIntercala(qtdArq));
+
+  teste(nomeArqResultante);
+
+  return 0;
 }
 
-void quickSort(long qtdRegistros, int numArq, FILE *entrada) {
+int compara(const void *e1, const void *e2) {
+  return strncmp(((Endereco *)e1)->cep, ((Endereco *)e2)->cep, 8);
+}
+
+void ordenacao(long qtdRegistros, int numArq, FILE *entrada) {
   int tamanhoSaida;
   FILE *saida;
   char nomeArq[32];
   int i;
-  
+
   for (i = 0; i < numArq; i++) {
     Endereco *e;
-    
+
     if (i == 0) {
       snprintf(nomeArq, sizeof(char) * 32, "cep-%d.dat", i + 1);
-      
-      saida = fopen(nomeArq, "wb");
-      
-      tamanhoSaida = ((qtdRegistros/numArq) + (qtdRegistros % numArq));
 
-      e = (Endereco*) malloc(tamanhoSaida * sizeof(Endereco));
-      
+      saida = fopen(nomeArq, "wb");
+
+      tamanhoSaida = ((qtdRegistros / numArq) + (qtdRegistros % numArq));
+
+      e = (Endereco *)malloc(tamanhoSaida * sizeof(Endereco));
+
       fread(e, sizeof(Endereco), tamanhoSaida, entrada);
 
       qsort(e, tamanhoSaida, sizeof(Endereco), compara);
-      
+
       fwrite(e, sizeof(Endereco), tamanhoSaida, saida);
     } else {
       snprintf(nomeArq, sizeof(char) * 32, "cep-%d.dat", i + 1);
-      
+
       saida = fopen(nomeArq, "wb");
-      
-      tamanhoSaida = qtdRegistros/numArq;
-      
-      e = (Endereco*) malloc(tamanhoSaida * sizeof(Endereco));
-      
+
+      tamanhoSaida = qtdRegistros / numArq;
+
+      e = (Endereco *)malloc(tamanhoSaida * sizeof(Endereco));
+
       fread(e, sizeof(Endereco), tamanhoSaida, entrada);
 
       qsort(e, tamanhoSaida, sizeof(Endereco), compara);
-      
+
       fwrite(e, sizeof(Endereco), tamanhoSaida, saida);
     }
 
@@ -62,82 +101,118 @@ void quickSort(long qtdRegistros, int numArq, FILE *entrada) {
   }
 }
 
-char *intercala(int numArq, char *arq1, int arq2) {
-  char nomeArq[32];
-  
-  snprintf(nomeArq, sizeof(char) * 32, "%s-%d", arq1, arq2);
-  
-  if (arq2 <= numArq) {
-    char nomeArqEntradaA[32];
-    char nomeArqEntradaB[32];
-    char nomeArqSaida[32];
-    
-    FILE *saida, *entradaA, *entradaB;
-    Endereco ea, eb;
-    
-    snprintf(nomeArqEntradaA, sizeof(char) * 32, "cep-%s.dat", arq1);
-    snprintf(nomeArqEntradaB, sizeof(char) * 32, "cep-%d.dat", arq2);
-    snprintf(nomeArqSaida, sizeof(char) * 32, "cep-%s.dat", nomeArq);
+char *criaNomeArquivosIntercala(int qtdArquivos) {
+  int i, j;
+  int n = 1, passos = 1, controle = 1;
+  char arq1[128] = "", arq2[128] = "";
 
-    entradaA = fopen(nomeArqEntradaA, "rb");
-    entradaB = fopen(nomeArqEntradaB, "rb");
-    saida = fopen(nomeArqSaida, "wb");
+  for (i = 1; i < ((qtdArquivos * 2) - 1); i++) {
+    int pos = 0;
 
-    fread(&ea, sizeof(Endereco), 1, entradaA);
-	  fread(&eb, sizeof(Endereco), 1, entradaB);
+    for (j = 0; j < passos; j++) {
+      if (controle == 1) {
+        pos += sprintf(&arq1[pos], "%d", n);
 
-    while(!feof(entradaA) && !feof(entradaB)) {
-  		if(compara(&ea,&eb)<0) {
-  			fwrite(&ea, sizeof(Endereco), 1, saida);
-  			fread(&ea, sizeof(Endereco), 1, entradaA);
-  		} else {
-			  fwrite(&eb, sizeof(Endereco), 1, saida);
-			  fread(&eb, sizeof(Endereco), 1, entradaB);
-		  }
-	  }
+        if (j < passos - 1) {
+          pos += sprintf(&arq1[pos], "-");
+        }
+      } else {
+        pos += sprintf(&arq2[pos], "%d", n);
 
-  	while(!feof(entradaA)) {
-  		fwrite(&ea, sizeof(Endereco), 1, saida);
-  		fread(&ea, sizeof(Endereco), 1, entradaA);		
-  	}
-    
-  	while(!feof(entradaB)) {
-  		fwrite(&eb, sizeof(Endereco), 1, saida);
-  		fread(&eb, sizeof(Endereco), 1, entradaB);		
-  	}
+        if (j < passos - 1) {
+          pos += sprintf(&arq2[pos], "-");
+        }
+      }
 
-    fclose(entradaA);
-    fclose(entradaB);
-    fclose(saida);
-    
-    snprintf(nomeArq, sizeof(char) * 32, "%s", intercala(numArq, nomeArq, arq2 + 1));
+      if (n == qtdArquivos) {
+        passos = passos * 2;
+        n = 1;
+        break;
+      }
+
+      n++;
+    }
+
+    if (controle == 2) {
+      char nomeArqA[128], nomeArqB[128], nomeArqSaida[128];
+
+      sprintf(&nomeArqA[0], "cep-%s.dat", arq1);
+      sprintf(&nomeArqB[0], "cep-%s.dat", arq2);
+      sprintf(&nomeArqSaida[0], "cep-%s-%s.dat", arq1, arq2);
+
+      controle = 0;
+
+      intercala(nomeArqA, nomeArqB, nomeArqSaida);
+
+      if (i == ((qtdArquivos * 2) - 2)) {
+        return nomeArqSaida;
+      }
+
+      strcpy(arq1, "");
+      strcpy(arq2, "");
+    }
+
+    controle++;
   }
-  
-  return nomeArq;
+
+  return "";
 }
 
-int main(void) {
+void intercala(char *nomeArqEntradaA, char *nomeArqEntradaB,
+               char *nomeArqSaida) {
+
+  FILE *saida, *entradaA, *entradaB;
+  Endereco ea, eb;
+
+  entradaA = fopen(nomeArqEntradaA, "rb");
+  entradaB = fopen(nomeArqEntradaB, "rb");
+  saida = fopen(nomeArqSaida, "wb");
+
+  fread(&ea, sizeof(Endereco), 1, entradaA);
+  fread(&eb, sizeof(Endereco), 1, entradaB);
+
+  while (!feof(entradaA) && !feof(entradaB)) {
+    if (compara(&ea, &eb) < 0) {
+      fwrite(&ea, sizeof(Endereco), 1, saida);
+      fread(&ea, sizeof(Endereco), 1, entradaA);
+    } else {
+      fwrite(&eb, sizeof(Endereco), 1, saida);
+      fread(&eb, sizeof(Endereco), 1, entradaB);
+    }
+  }
+
+  while (!feof(entradaA)) {
+    fwrite(&ea, sizeof(Endereco), 1, saida);
+    fread(&ea, sizeof(Endereco), 1, entradaA);
+  }
+
+  while (!feof(entradaB)) {
+    fwrite(&eb, sizeof(Endereco), 1, saida);
+    fread(&eb, sizeof(Endereco), 1, entradaB);
+  }
+
+  fclose(entradaA);
+  fclose(entradaB);
+
+  remove(nomeArqEntradaA);
+  remove(nomeArqEntradaB);
+
+  fclose(saida);
+}
+
+void teste(char *nomeArqResultante) {
   FILE *entrada;
-  long tamanho, qtdRegistros;
-  int n;
+  Endereco e;
+  int i;
 
-  printf("Digite a quantidade de divisões no arquivo: ");
-  scanf("%d", &n);
+  entrada = fopen(nomeArqResultante, "rb");
 
-  entrada = fopen("cep.dat", "rb");
+  fread(&e, sizeof(Endereco), 1, entrada);
 
-  fseek(entrada, 0, SEEK_END);
-  
-  tamanho = ftell(entrada);
-  qtdRegistros = tamanho / sizeof(Endereco);
+  for (i = 0; i < 50; i++) {
+    printf("\n%.8s", e.cep);
+    fread(&e, sizeof(Endereco), 1, entrada);
+  }
 
-  rewind(entrada);
-
-  quickSort(qtdRegistros, n, entrada);
-  
   fclose(entrada);
-  
-  intercala(4, "1", 2);
-    
-  return 0;
 }
